be21f8bbeaeb6d1fe28cfdf10039470446e30cda
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-17 13:24:25 +0600

    first commit

---
+++ src/main/java/kg/attractor/java25/microgram/Lw69MicrogramApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.java25.microgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lw69MicrogramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lw69MicrogramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/java25/microgram/Lw69MicrogramApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.java25.microgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lw69MicrogramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
