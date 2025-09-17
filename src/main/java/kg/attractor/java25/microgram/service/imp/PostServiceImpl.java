8c906a6fe7cff3ba44355100822e0142021eecea
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-17 20:32:06 +0600

    edit

--- src/main/java/kg/attractor/java25/microgram/service/imp/PostServiceImpl.java
+++ src/main/java/kg/attractor/java25/microgram/service/imp/PostServiceImpl.java
@@ -1,10 +1,8 @@
 package kg.attractor.java25.microgram.service.imp;
 
-import kg.attractor.java25.microgram.dto.image.PostDto;
 import kg.attractor.java25.microgram.dto.image.PostUpsertDto;
 import kg.attractor.java25.microgram.model.Post;
 import kg.attractor.java25.microgram.repository.PostRepository;
-import kg.attractor.java25.microgram.service.ImageService;
 import kg.attractor.java25.microgram.service.PostService;
 import kg.attractor.java25.microgram.service.UserService;
 import kg.attractor.java25.microgram.util.FileUtil;
@@ -13,50 +11,28 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
-
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
     private final UserService userService;
-    private final ImageService imageService;
 
+
+    @Transactional
     @Override
-    public void createPost(PostUpsertDto dto) {
+    public Long createPost(PostUpsertDto dto, MultipartFile image) {
+        if (image == null || image.isEmpty()) {
+            throw new IllegalArgumentException("Image is required");
+        }
 
+        String imagePath = FileUtil.saveUploadedFile(image, "image");
         Post post = new Post();
         post.setAuthor(userService.getUserById(dto.getAuthorId()));
-        post.setImage(dto.getImage());
         post.setDescription(dto.getDescription());
+        post.setImage(imagePath);
+        Post saved = postRepository.save(post);
 
-        postRepository.save(post);
-    }
 
-    @Override
-    public Long createPostWithImage(MultipartFile image, String description, String name) {
-        return 0L;
+        return saved.getId();
     }
-//
-//    @Transactional
-//    @Override
-//    public PostDto createPostWithImage(PostUpsertDto dto, MultipartFile image) {
-//        if (image == null || image.isEmpty()) {
-//            throw new IllegalArgumentException("Изображение обязательно");
-//        }
-//
-//        String fileName = FileUtil.saveImage(image);
-//
-//        Post post = new Post();
-//        post.setAuthor(dto.getAuthorId());
-//        post.setDescription(dto.description());
-//        post.setCreatedAt(LocalDateTime.now());
-//        post.setCommentsCount(0);
-//        post.setLikesCount(0);
-//        post.setImage(fileName);
-//
-//        postRepository.save(post);
-//
-//        return PostDto.from(post);
-//    }
 }
