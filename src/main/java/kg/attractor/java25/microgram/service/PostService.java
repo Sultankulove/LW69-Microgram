dea4e320421b604a59678f299db747f39034bff7
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-17 20:31:52 +0600

    edit

--- src/main/java/kg/attractor/java25/microgram/service/PostService.java
+++ src/main/java/kg/attractor/java25/microgram/service/PostService.java
@@ -1,6 +1,5 @@
 package kg.attractor.java25.microgram.service;
 
-import kg.attractor.java25.microgram.dto.image.PostDto;
 import kg.attractor.java25.microgram.dto.image.PostUpsertDto;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,7 +8,4 @@ public interface PostService {
 
     @Transactional
     Long createPost(PostUpsertDto dto, MultipartFile image);
-
-    Long createPostWithImage(MultipartFile image, String description, String name);
-
 }
