e77e455c8e4878082284f13384044f0483f37b4d
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-18 13:05:08 +0600

    edit

---
+++ src/main/java/kg/attractor/java25/microgram/dto/PostsListViewDto.java
@@ -0,0 +1,14 @@
+package kg.attractor.java25.microgram.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostsListViewDto {
+
+}
