7852f247af352b47a006932497adae4aa3fe432e
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-19 15:06:04 +0600

    edit

--- src/main/java/kg/attractor/java25/microgram/controller/PostController.java
+++ src/main/java/kg/attractor/java25/microgram/controller/PostController.java
@@ -8,10 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
